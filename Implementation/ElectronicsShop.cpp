ef3749b48537fca6a8eb02f8b47cfc6b613a733f
Orsted <cuong.vm.21@gmail.com>
2025-02-12 16:28:15 +0700

    my solution for Electronics Shop (brute force)

---
+++ Implementation/ElectronicsShop.cpp
@@ -0,0 +1,34 @@
+/***********************************************
+ * File:        ElectronicsShop.cpp
+ * Date:        2025 Feb 12 16:02:57
+ * Description: Basic C++ program template
+ ***********************************************/
+#include <iostream>
+
+using namespace std;
+#define ll long long
+
+int main(int argc, char **argv) {
+	int b, n, m, max = 0;
+
+	cin >> b >> n >> m;
+	int k[n], d[m];
+	for (int i = 0; i < n; ++i)
+		cin >> k[i];
+	for (int i = 0; i < m; ++i)
+		cin >> d[i];
+
+	for (int i : k) {
+		for (int j : d) {
+			if (i + j <= b)
+				max = (i + j > max) ? i + j : max;
+		}
+	}
+
+	if (max != 0)
+		cout << max;
+	else
+		cout << "-1";
+
+	return 0;
+}
