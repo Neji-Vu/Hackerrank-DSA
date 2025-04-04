57a0941e4ebb164346ebb6d5cd2ae35314c7fa10
Orsted <cuong.vm.21@gmail.com>
2025-04-04 23:01:54 +0700

    solving Poisonous Plants problem

--- Stacks/PoisonousPlants.cpp
+++ Stacks/PoisonousPlants.cpp
@@ -11,17 +11,7 @@ using namespace std;
 #define ll long long
 const int mxN = 1e5;
 
-int main(int argc, char **argv) {
-	int n;
-	vector<int> p;
-
-	cin >> n;
-	for (int i = 0; i < n; ++i) {
-		int temp;
-		cin >> temp;
-		p.push_back(temp);
-	}
-
+int mySolution(vector<int> p) {
 	stack<int> st;
 	int cnt = 0, ans = 0, size = p.size();
 	do {
@@ -41,7 +31,39 @@ int main(int argc, char **argv) {
 		}
 	} while (cnt + 1 != size);
 
-	cout << ans;
+	return ans;
+}
+
+int OptimalSolution(vector<int> v) {
+	stack<pair<int, int>> st;
+	int ans = 0;
+	// vector<int> span(v.size(), -1);
+
+	for (int i = 0; i < v.size(); i++) {
+		int cnt = 1;
+		while (!st.empty() && v[st.top().first] >= v[i]) {
+			cnt = max(cnt, st.top().second + 1);
+			st.pop();
+		}
+		if (st.empty())
+			cnt = 0;
+		ans = max(ans, cnt);
+		st.push({i, cnt});
+	}
+
+	return ans;
+}
+
+int main(int argc, char **argv) {
+	int n;
+
+	cin >> n;
+	vector<int> p(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> p[i];
+	}
+
+	cout << OptimalSolution(p);
 
 	return 0;
 }
